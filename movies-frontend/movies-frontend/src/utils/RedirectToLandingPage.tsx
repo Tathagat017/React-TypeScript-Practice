642ad3e365c0ca9c1d983b4125fb46de8405b2f0
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-09-24 21:17:55 +0530

    final push for today

--- movies-frontend/movies-frontend/src/utils/RedirectToLandingPage.tsx
+++ movies-frontend/movies-frontend/src/utils/RedirectToLandingPage.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 const RedirectToLandingPage = () => {
   const [time, setTime] = useState<number>(5);
@@ -20,8 +21,9 @@ const RedirectToLandingPage = () => {
 
   return (
     <>
-      <h3>Page not found 404 </h3>
-      <p>Returning to main page in : {time}</p>
+      <h3>Page not found : 404 </h3>
+      <p>Redirecting to main page in : {time}</p>
+      <NavLink to={"/"}> Home </NavLink>
     </>
   );
 };
