9ca562305d94e1a8fa3b694fb17e0cf452e3cd9b
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-09-24 21:13:40 +0530

    final redirect

--- movies-frontend/movies-frontend/src/utils/RedirectToLandingPage.tsx
+++ movies-frontend/movies-frontend/src/utils/RedirectToLandingPage.tsx
@@ -1,9 +1,27 @@
 import { Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const RedirectToLandingPage = () => {
+  const [time, setTime] = useState<number>(5);
+
+  useEffect(() => {
+    var timeout = setInterval(() => {
+      setTime((prev) => prev - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timeout);
+    };
+  }, []);
+
+  if (time === 0) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <>
-      <Navigate to={"/"} />
+      <h3>Page not found 404 </h3>
+      <p>Returning to main page in : {time}</p>
     </>
   );
 };
